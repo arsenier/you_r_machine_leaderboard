00997dbaf7ce6ca10845007006082e474e1b1bab
Arseniy <yarmolinskiyam@gmail.com>
2024-05-20 14:44:18 +0300

    score keeping works

---
+++ api_v1/scores/dependencies.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Path, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import db_helper, Score
+
+from . import crud
+
+
+async def score_by_id(
+    score_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> Score:
+    product = await crud.get_score(session=session, score_id=score_id)
+    if product is not None:
+        return product
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Score {score_id} not found!",
+    )
--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from fastapi import FastAPI
 
 import uvicorn
 
+from api_v1 import router as router_v1
+from core.config import settings
+from core.models import db_helper, Base
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # async with db_helper.engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.create_all)
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
 app = FastAPI(lifespan=lifespan)
-# app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 # app.include_router(items_router)
 # app.include_router(users_router)
 
